2824f21b7f455c452a993e6f5ec5228f070133e9
unknown <2fixelectronics777@gmail.com>
2022-03-31 22:56:23 -0500

    add ref finished mod

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useRef} from 'react'
+import {useState, useRef} from 'react'
 import './App.css';
 import SearchBar from './components/SearchBar';
 import Gallery from './components/Gallery'
@@ -8,13 +8,14 @@ import { DataContext } from './context/DataContext'
 import { SearchContext } from './context/SearchContext'
 
 function App() {
-    let [search, setSearch] = useState('')
+    //let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
     let searchInput = useRef('')
 
     const API_URL = 'https://itunes.apple.com/search?term='
 
+    /*
     useEffect(() => {
         if (search) {
             //console.log(term)
@@ -31,10 +32,24 @@ function App() {
             fetchData()
         }
     },[search])
+    */
+
 
     const handleSearch = (e, term) => {
         e.preventDefault()
-        setSearch(term)
+        // fetch data
+        const fetchData = async () => {
+            document.title = `${term} Music`
+            const response = await fetch(API_URL + term)
+            const resData = await response.json()
+            if (resData.results.length > 0) {
+                setData(resData.results)
+            } else {
+                setMessage('Not Found')
+            }
+        }
+        fetchData()
+        
     }
 
   return (
